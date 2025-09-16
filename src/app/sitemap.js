414e66830bbdf3dd45200ac7a85f594c7048be3b
Pedro Segura <51924485+psegurap@users.noreply.github.com>
2025-09-16 12:54:42 -0400

    added 'prueba' to sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,19 +1,36 @@
 import available_states from "@/jsons/states.json";
 
-const states = available_states.map((state) => {
-    return {
+let estados = [];
+
+available_states.forEach((state) => {
+    estados.push({
         url: `${process.env.NEXT_PUBLIC_BASE_URL}/estado/${state.slug}`,
-    };
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 0.8,
+    });
+    estados.push({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/estado/${state.slug}/prueba?preguntas=20`,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+    });
 });
 
 export default function sitemap() {
     return [
         {
             url: process.env.NEXT_PUBLIC_BASE_URL,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1,
         },
+        ...estados,
         {
             url: process.env.NEXT_PUBLIC_BASE_URL + "/privacidad",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.3,
         },
-        ...states,
     ];
 }
